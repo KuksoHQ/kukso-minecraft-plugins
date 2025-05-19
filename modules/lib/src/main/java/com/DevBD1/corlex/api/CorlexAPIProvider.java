e3b2934118682c21e0613e66770d7992cf30702c
BurakDorman <burakdorman@gmail.com>
2025-05-19 13:48:39 +0300

    improved API

--- modules/lib/src/main/java/com/DevBD1/corlex/api/CorlexAPIProvider.java
+++ modules/lib/src/main/java/com/DevBD1/corlex/api/CorlexAPIProvider.java
@@ -1,17 +1,20 @@
 package com.DevBD1.corlex.api;
 
-import org.bukkit.Bukkit;
-
 public class CorlexAPIProvider {
+
     private static CorlexAPI instance;
 
+    public static void register(CorlexAPI api) {
+        if (instance != null) throw new IllegalStateException("CorlexAPI already registered");
+        instance = api;
+    }
+
     public static CorlexAPI get() {
-        if (instance == null) {
-            instance = Bukkit.getServicesManager().load(CorlexAPI.class);
-            if (instance == null) {
-                throw new IllegalStateException("CorlexAPI is not available. Is the plugin loaded?");
-            }
-        }
+        if (instance == null) throw new IllegalStateException("CorlexAPI is not available yet");
         return instance;
+    }
+
+    public static boolean isAvailable() {
+        return instance != null;
     }
 }
