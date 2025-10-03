3d9f56f1ca26e31f5f0c6d8a389d12ddd3824373
BurakDorman <burakdorman@gmail.com>
2025-10-03 17:50:08 +0300

    Removed PluginInitializer.java

--- modules/lib/src/main/java/io/github/devbd1/cublexcore/Main.java
+++ modules/lib/src/main/java/io/github/devbd1/cublexcore/Main.java
@@ -1,18 +1,51 @@
 package io.github.devbd1.cublexcore;
 
+import io.github.devbd1.cublexcore.commands.CommandRegistrar;
+import io.github.devbd1.cublexcore.hooks.HookRegistrar;
+import io.github.devbd1.cublexcore.modules.ModuleRegistrar;
+import io.github.devbd1.cublexcore.services.ServiceRegistrar;
+import io.github.devbd1.cublexcore.services.ServiceUnregistrar;
+
+import io.github.devbd1.cublexcore.modules.logger.LoggingManager;
+import io.github.devbd1.cublexcore.modules.text.Lang;
+import io.github.devbd1.cublexcore.services.CorlexAPIImplementer;
+import io.github.devbd1.cublexcore.utilities.*;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    private PluginInitializer bootstrap;
+
+    private LoggingManager logger;
+    private CorlexAPIImplementer api;
 
     @Override
     public void onEnable() {
-        bootstrap = new PluginInitializer(this);
-        bootstrap.enable();
+        // --- config & localization
+        saveDefaultConfig();
+        ConfigManager.init(this);
+        Lang.load(this);
+        ConfigManager.printStatus();
+
+        // --- core services
+        logger = new LoggingManager(this);
+        api    = new CorlexAPIImplementer(this, logger);
+        ServiceRegistrar.registerAll(this, api, logger);
+
+        // --- commands
+        CommandRegistrar.register(this, logger);
+
+        // --- hooks (PlaceholderAPI, CubItems, RealisticSeasons…)
+        HookRegistrar.register(this, logger);
+
+        // ** ProtocolLib packet‐translation hook **
+        ModuleRegistrar.register(this, api, logger);
+
+        logger.log("CublexCore is fully enabled.");
     }
 
     @Override
     public void onDisable() {
-        bootstrap.disable();
+        ServiceUnregistrar.unregisterAll(this, api, logger);
+        logger.log("CublexCore is disabled.");
     }
 }
